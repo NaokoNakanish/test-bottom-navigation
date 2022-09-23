b6f2da68fe3be2286da0a4b3f73e66b2677d86df
Naoko Nakanishi <fu2fuka.nao@gmail.com>
2022-09-23 16:27:28 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const MODE = "production";
+
+const webpack = require("@nativescript/webpack");
+const { DefinePlugin } = require("webpack");
+const { resolve } = require("path");
+
+const path = require("path");
+
+module.exports = (env) => {
+  webpack.init(env);
+
+  // Learn how to customize:
+  // https://docs.nativescript.org/webpack
+
+  const {
+    production, // --env.production
+  } = env;
+  const mode = production ? "production" : "development";
+  const platform =
+    env && ((env.android && "android") || (env.ios && "ios") || env.platform);
+
+  webpack.chainWebpack((config) => {
+    config.plugin("DefinePlugin").use(DefinePlugin, [
+      {
+        TNS_ENV: JSON.stringify(mode),
+        "global.TNS_WEBPACK": "true",
+        "global.isAndroid": platform === "android",
+        "global.isIOS": platform === "ios",
+        process: "global.process",
+
+        __UI_USE_XML_PARSER__: true,
+        __UI_USE_EXTERNAL_RENDERER__: false,
+        __CSS_PARSER__: JSON.stringify("css-tree"),
+      },
+    ]);
+  });
+
+  return webpack.resolveConfig();
+};
